ced525fffb14dc1ebfa703c9f4edb79c9fbea87f
Aline Leroy <aline.leroy.27@gmail.com>
2021-03-23 13:41:42 +0100

    finition du CRUD et correction du bug avec ng serve

--- app.js
+++ app.js
@@ -43,6 +43,18 @@ app.get('/api/sauces/:id', (req, res, next) => {
     .catch(error => res.status(404).json({ error }));
 });
 
+app.put('/api/sauces/:id', (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/sauces/:id', (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.use('/api/sauces', (req, res, next) => {
     const stuff = [
       {
